5753ab7ec46bed10a53973ce8a7038ba6e98a8ba
tbliu0121 <tbliu0121@foxmail.com>
2021-06-22 20:59:33 +0800

    defineReaction()将数据属性设置 为响应式数据

---
+++ 08_Vue/Day10(responsive-data)/part_01/src/defineReactive.js
@@ -0,0 +1,27 @@
+import observe from "./observe.js";
+
+// 利用闭包的特性, 将defineProperty中需要中转变量完成数据的特点改进并封装。
+export default function defineReactive(obj, attr, value) {
+  console.log("Here is defineReaction()", obj, attr);
+
+  if(arguments.length === 2) {
+    value = obj[attr];
+  }
+
+  let attrObj = observe(value);  // 如果属性是一个
+
+  Object.defineProperty(obj, attr, {
+    get() {
+      console.log(`访问了对象的${attr}属性`);
+      return value;
+    },
+    set(newValue) {
+      console.log(`设置了的对象的${attr}属性为: ${newValue}`);
+      if(newValue == value) {
+        return;
+      }
+      value = newValue;
+      attrObj = observe(newValue);  // 设置的新值需要成为被观察的属性
+    }
+  });
+}
