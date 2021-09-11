5ee822f880185ba5420e0a819c9ab598fb94f412
tbliu0121 <tbliu0121@foxmail.com>
2021-09-11 21:34:13 +0800

    传送门： <eleport></teleport>

---
+++ 11_Vue3/Day01/vue3_start/20_src-teleport/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+<section class="child">
+  <h3>我是一个子组件</h3>
+  <slot></slot>
+</section>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Child',
+})
+</script>
+
+
+<style scoped>
+.child {
+  padding: 10px;
+  background: teal;
+}
+</style>
---
+++ 11_Vue3/Day01/vue3_start/20_src-teleport/components/GrandSon.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="grand-son">
+    <h3>我是一个后代组件</h3>
+    <Dialog/>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Dialog from "./Dialog";
+
+export default defineComponent({
+  name: "GrandSon",
+  components: {
+    Dialog,
+  },
+});
+</script>
+
+<style scoped>
+.grand-son {
+  padding: 10px;
+  background: tomato;
+}
+</style>
